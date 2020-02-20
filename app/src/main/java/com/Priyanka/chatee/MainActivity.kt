e51172158382399fe526d60fc3d70784caa11be2
Priyanka <priyakammari21@gmail.com>
2020-02-20 12:43:29 -0800

    login and register

--- app/src/main/java/com/Priyanka/chatee/MainActivity.kt
+++ app/src/main/java/com/Priyanka/chatee/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.Priyanka.chatee
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,9 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        login_button.setOnClickListener {
+            startActivity(AuthActivity.newIntent(AuthMode.Login, this))
+        }
 
+        register_button.setOnClickListener {
+            startActivity(AuthActivity.newIntent(AuthMode.Register, this))
+        }
         val database=FirebaseDatabase.getInstance()
         val myRef=database.getReference("message")
         myRef.setValue("Hello World!")
     }
+}
+
+
+sealed class AuthMode() : Parcelable {
+    @Parcelize
+    object Login : AuthMode()
+
+    @Parcelize
+    object Register : AuthMode()
 }
